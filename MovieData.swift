9cee1f4faf58a200b4869c53bc3f8155b36193b7
Anna Nikiforova <annanikiforova19@mail.ru>
2020-03-12 22:12:11 +0300

    refactoring

--- MovieData.swift
+++ MovieData.swift
@@ -8,26 +8,15 @@
 
 import Foundation
 
-class MovieData: Decodable {
+struct MovieData: Decodable {
     let results: [MovieInfo]
-    
-    init(results: [MovieInfo]) {
-        self.results = results
-    }
 }
 
-class MovieInfo: Decodable {
+struct MovieInfo: Decodable {
     let title: String
     let overview: String
     let poster_path: String
     let release_date: String
     let vote_average: Double
-    
-    init(title: String, overview: String, poster_path: String, release_date: String, vote_average: Double) {
-        self.title = title
-        self.overview = overview
-        self.poster_path = poster_path
-        self.release_date = release_date
-        self.vote_average = vote_average
-    }
 }
+
